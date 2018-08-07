2eb91868731c19f3c060fc82ff76ceabb21d6cff
haoshuaiwei <haoshuaiwei@jd.com>
2018-08-07 11:28:49 +0800

    fix: delete doc

--- go_php_func/doc.go
+++
@@ -1,8 +0,0 @@
-// Copyright 2018 Openset. All rights reserved.
-// Use of this source code is governed by a MIT
-// license that can be found in the LICENSE file.
-
-/*
-PHP built-in function library with Go language
-*/
-package go_php_func // import "github.com/openset/php2go/php"
